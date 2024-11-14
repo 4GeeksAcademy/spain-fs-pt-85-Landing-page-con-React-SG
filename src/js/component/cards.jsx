6c54b68d994048d1f86ff3d361ae40e4d2065e64
Sergi-03 <robrovskyyt@gmail.com>
2024-11-14 12:36:00 +0000

    finalizado

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,58 +1,59 @@
 import React from "react";
 
 function Cards(){
-    return  <div className="content mx-auto col-11 col-md-10" id="portfolio" style={{position: "relative", bottom: "40px"}}>
-    <div className="container">
-        <div className="row d-flex justify-content-center">
-            <div className="project col-12 col-md-6 col-lg-4">
-                <div className="project-img mb-3">
-                    <img className="object-fit-cover w-100" src="" alt="">
-                    </img>
-                </div>
-                <div className="project-body">
-                    <h5 className="project-name">
-                        Card title
-                    </h5>
-                    <p className="project-comment">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
-                    </p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div className="project col-12 col-md-6 col-lg-4">
-                <div className="project-img mb-3">
-                    <img className="object-fit-cover w-100" src="" alt="">
-                    </img>
-                </div>
-                <div className="project-body">
-                    <h5 className="project-name">
-                        Card title
-                    </h5>
-                    <p className="project-comment">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
-                    </p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div className="project col-12 col-md-6 col-lg-4">
-                <div className="project-img mb-3">
-                    <img className="object-fit-cover w-100" src="" alt="">
-                    </img>
-                </div>
-                <div className="project-body">
-                    <h5 className="project-name">
-                        Card title
-                    </h5>
-                    <p className="project-comment">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
-                    </p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+    return  <div className="container-fluid text-center" style={{position: "relative", bottom: "60px"}}>
+    <div className="row justify-content-center row-cols-1 row-cols-md-4 g-4">
+      <div className="col">
+        <div className="card">
+          <img src="https://imgs.search.brave.com/1VknSsSZ7FbKv-yB46WKTe77s1YmmM6zWY4DOFifXqg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzM2Lzg0Lzgz/LzM2MF9GXzIzNjg0/ODM0MV8wQjQ2VDZ5/cGhKbURDZkNManJV/QktjcXdHRGJ3ZHQ2/VC5qcGc" className="card-img-top" alt="...">
+          </img>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text" style={{fontSize: "10px"}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
+            </p>
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+          </div>
         </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="https://imgs.search.brave.com/Lt2OrLEBsHEIrUg6zlZFzsfUS7QqSg8f3pnyZZ01luA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L2xvdmVseS12aW50/YWdlLWZpYXQtNTAw/LXBhcmtlZC1pbi10/aGUtcGlhenphLWRp/LXNwYWduYS1kaXN0/cmljdC1pbi1waWN0/dXJlLWlkMTM3MTI0/MzQ3ND9iPTEmaz0y/MCZtPTEzNzEyNDM0/NzQmcz0xNzA2Njdh/Jnc9MCZoPTNyRjlz/dWd4Zy1rVFdKV2Vo/MktKM1dsckstUEoz/UFhNMVRiZldOYWE4/bFk9.jpeg" className="card-img-top" alt="...">
+          </img>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text" style={{fontSize: "10px"}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
+            </p>
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="https://imgs.search.brave.com/cUNOGNhlNd-MBkBa0SHiCnprJV7LOKWAxLf8miGso3A/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L2ZpYXQtNTAwLWNy/ZWFtLWNvbG9yLXBp/Y3R1cmUtaWQxMzUy/Njg4NTY3P2I9MSZr/PTIwJm09MTM1MjY4/ODU2NyZzPTE3MDY2/N2Emdz0wJmg9NzRr/YW5XdlIxYjVTdFQy/UGg2WS05TDFiZXBn/NzI0QkdhRkZlR2lm/U0dJaz0.jpeg" className="card-img-top" alt="...">
+          </img>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text" style={{fontSize: "10px"}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
+            </p>
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img src="https://imgs.search.brave.com/LLjbJ9ibVlLZNP8AlFsilqRZF1SerbGNzsuLLmuOtwk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L2ZpYXQtNTAwLWVu/Z2luZS1waWN0dXJl/LWlkNDg0Mzc3ODA1/P2I9MSZrPTIwJm09/NDg0Mzc3ODA1JnM9/MTcwNjY3YSZ3PTAm/aD1zUGZLd3ZEdXY5/NnZNaVJqZlpVc21S/aEczTy1lRkdQdWZD/azl3RWo2N1RZPQ.jpeg" className="card-img-top" alt="...">
+          </img>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text" style={{fontSize: "10px"}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime dolorum nihil harum voluptates quod, in magnam fuga dignissimos saepe maiores placeat tenetur ipsa nemo similique voluptatibus illo nostrum, accusamus exercitationem.
+            </p>
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-
+  </div>
+  
 }
 
 export default Cards;
